2b73096291b4fce0e053d1ea51722a4a268cf7dc
Suryansh1311 <suryanshagarwal13@gmail.com>
2026-01-15 22:50:40 +0800

    allow vercel

--- api/main.py
+++ api/main.py
@@ -33,7 +33,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],   # keep for local dev
+    allow_origins=["http://localhost:3000", "https://hkust-timetable-optimizer.vercel.app/"],   # keep for local dev
     allow_origin_regex=r"https://.*\.vercel\.app",  # allow all Vercel preview/prod URLs
     allow_credentials=True,
     allow_methods=["*"],
